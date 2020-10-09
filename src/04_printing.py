f4e27d5612b24f9254d04fa508deaebf35cec0e3
Yue Su <su2047@gmail.com>
2020-10-09 20:29:17 -0300

    added tuples

--- src/04_printing.py
+++ src/04_printing.py
@@ -9,6 +9,7 @@ y = 2.24552
 z = "I like turtles!"
 
 # Using the printf operator (%), print the following feeding in the values of x,
+print("x is %2d, y is %4.2f, z is %s" %(x, y, z))
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
@@ -16,4 +17,7 @@ z = "I like turtles!"
 
 # Use the 'format' string method to print the same thing
 
+print("x is {}, y is {:4.2f}, z is {}" .format(x, y, z))
+
 # Finally, print the same thing using an f-string
+
