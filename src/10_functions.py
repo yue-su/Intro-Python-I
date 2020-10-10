571f35e1f53d08d371dce7fbcb70f14c43338158
Yue Su <su2047@gmail.com>
2020-10-10 00:12:09 -0300

    added functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,28 @@
 
 # YOUR CODE HERE
 
+def check_even(x):
+    if x % 2 == 0:
+        return True
+    return False
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+result = check_even(num)
+print(result)
+
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
+def check_even_se(x):
+    if x % 2 == 0:
+        print("even!")
+    else:
+        print("Odd!")
 
+num = input("enter another number: ")
+num = int(num)
+print(check_even_se(num))
+# YOUR CODE HERE
