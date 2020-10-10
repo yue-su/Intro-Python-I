51b7c3e57b457ef5ff6632a3d280c7404dacadbb
Yue Su <su2047@gmail.com>
2020-10-10 12:52:09 -0300

    added functions

--- src/11_args.py
+++ src/11_args.py
@@ -5,8 +5,12 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return x + y
+
 
 print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
@@ -14,15 +18,23 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
+def f2( *args):
+    result = 0
+    for arg in args:
+        result += arg
+    return result
+
+
+print(f2(1))  # Should print 1
+print(f2(1, 3))  # Should print 4
+print(f2(1, 4, -12))  # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+
+print(f2(a[0],a[1],a[2],a[3]))  # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,10 +42,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y = 1):
+    return x + y
 
 print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
-
+print(f3(8))  # Should print 9
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
@@ -44,6 +57,11 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(a, b):
+    print("key a, value {}" .format(a))
+    print("key b, value {}" .format(b))
+
+
 
 # Should print
 # key: a, value: 12
