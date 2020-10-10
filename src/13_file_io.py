8ac863da249c2cfc171d20c364b2d630de508ab7
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-10-10 14:43:03 -0300

    added file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+f = open('foo.txt', 'r')
+print(f.read())
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
+
+bar = open('bar.txt', 'w')
+bar.write('This is some text\n some other stuff\n last but not the least\n')
+
 
 # YOUR CODE HERE
